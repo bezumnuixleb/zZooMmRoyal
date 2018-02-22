4669e5cef9770de6463dec03c7b22dff2d883bde
zZooMm <yaroslav-aleksev-1999@mail.ru>
2018-02-22 20:42:59 +0300

    Моб + перс

--- zZooMm/Game1.cs
+++ zZooMm/Game1.cs
@@ -12,6 +12,8 @@ namespace zZooMm001
         SpriteBatch spriteBatch;
 
         private List<Plain> _Plain;
+        private List<MobZombie> _Zombie;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -22,10 +24,7 @@ namespace zZooMm001
 
         protected override void Initialize()
         {
-
-
             base.Initialize();
-          
         }
 
         protected override void LoadContent()
@@ -34,14 +33,17 @@ namespace zZooMm001
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             var texture = Content.Load<Texture2D>("plain");
+            var textureZ = Content.Load<Texture2D>("Zombie");    
 
             _Plain = new List<Plain>()
             {
-                new Plain(texture) {_position = new Vector2(100,100), _speed = 3f, _input = new Input { Left = Keys.A, Right = Keys.D, Up = Keys.W, Down = Keys.S}, _Size = new Vector2(0.1f,0.15f), Origin = new Vector2(texture.Width/2,texture.Height/2),_rotation = 0},
-                //new Plain(texture) {_position = new Vector2(0,0), _speed = 2f, _input = new Input { Left = Keys.Left, Right = Keys.Right, Up = Keys.Up, Down = Keys.Down}, _Size = new Vector2(1f,1f) },
-
+                new Plain(texture) {_position = new Vector2(600,600), _speed = 5f, _input = new Input { Left = Keys.A, Right = Keys.D, Up = Keys.W, Down = Keys.S}, _Size = new Vector2(0.1f,0.15f), Origin = new Vector2(texture.Width/2,texture.Height/2),_rotation = 0},
             };
-
+            _Zombie = new List<MobZombie>()
+            {
+                new MobZombie(textureZ) {_position = new Vector2(100,100), _speed = 2f, _Size = new Vector2(0.2f,0.2f), Origin = new Vector2(textureZ.Width/2,textureZ.Height/2),_rotation = 0},
+                new MobZombie(textureZ) {_position = new Vector2(0,0), _speed = 2f, _Size = new Vector2(0.3f,0.3f), Origin = new Vector2(textureZ.Width/2,textureZ.Height/2),_rotation = 0},
+            };
         }
 
 
@@ -57,6 +59,9 @@ namespace zZooMm001
             foreach (var plain in _Plain)
                 plain.Update();
 
+            foreach (var zombie in _Zombie)
+                zombie.Update(_Plain, 300);
+
             base.Update(gameTime);
         }
 
@@ -68,6 +73,9 @@ namespace zZooMm001
 
             foreach (var plain in _Plain)
                 plain.Draw(spriteBatch);
+
+            foreach (var zombie in _Zombie)
+                zombie.Draw(spriteBatch);
 
             spriteBatch.End();
             base.Draw(gameTime);
---
+++ zZooMm/MobZombie.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zZooMm001
+{
+    class MobZombie
+    {
+        private Texture2D _texture;
+        public Vector2 Origin;
+        public Vector2 _position;
+        public Vector2 _Size;
+
+        public float _speed = 2f;
+
+        public float _rotation = 3f;
+
+        public MobZombie(Texture2D texture)
+        {
+            _texture = texture;
+        }
+        public void Update(List<Plain> Plain, float distanceMin)
+        {
+
+            foreach (var plain in Plain)
+            {
+                double formDistance = (double)((plain._position.X - _position.X) * (plain._position.X - _position.X) + (plain._position.Y - _position.Y) * (plain._position.Y - _position.Y));
+                float distance = (float)Math.Sqrt((double)formDistance);
+                if (distanceMin > distance)
+                {
+                    // поворот к персонажу 
+                    Vector2 mousePosition = new Vector2(plain._position.X, plain._position.Y);
+
+                    Vector2 direction = mousePosition - _position;
+                    direction.Normalize();
+
+                    _rotation = (float)Math.Atan2((double)direction.Y, (double)direction.X);
+
+                    // движение к персонажу 
+
+                    var directory = new Vector2((float)Math.Cos(MathHelper.ToRadians(90) - _rotation), -(float)Math.Sin(MathHelper.ToRadians(90) - _rotation));
+                    _position += direction * _speed;
+                    //_position.X = _speed * (plain._position.X - _position.X) / distance;
+                    //_position.Y = _speed * (plain._position.Y - _position.Y) / distance;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _position, null, Color.White, _rotation, Origin, _Size, SpriteEffects.None, 0f);
+        }
+    }
+}
