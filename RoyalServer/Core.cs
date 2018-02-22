ab4edf4c858b98219e522d0738c79c696ae4e76a
Bezumnuixleb <bezumnui32@gmail.com>
2018-02-22 14:29:51 +0300

    NachaloPolojeno(net)

---
+++ RoyalServer/Core.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalServer
+{
+    class Object
+    {
+        protected Texture2D _texture;
+        public Vector2 _position;
+        public String _Type;
+        public Object(Texture2D ntext)
+        {
+            _texture = ntext;
+        }
+        public virtual void Update(GameTime gameTime,List<Object> gameobj)
+        {
+
+        }
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
