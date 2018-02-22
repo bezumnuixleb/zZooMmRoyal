3ebf16a210924eda14298d8fd61dc468cb358c8f
zZooMm <yaroslav-aleksev-1999@mail.ru>
2018-02-22 19:31:08 +0300

    Поворот

--- zZooMm/Game1.cs
+++ zZooMm/Game1.cs
@@ -20,54 +20,36 @@ namespace zZooMm001
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+
 
             base.Initialize();
           
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
+
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            var texture = Content.Load<Texture2D>("test");
+            var texture = Content.Load<Texture2D>("plain");
 
             _Plain = new List<Plain>()
             {
-                new Plain(texture) {_position = new Vector2(100,100), _speed = 3f, _input = new Input { Left = Keys.A, Right = Keys.D, Up = Keys.W, Down = Keys.S}, _Size = new Vector2(3f,3f) },
-               new Plain(texture) {_position = new Vector2(0,0), _speed = 2f, _input = new Input { Left = Keys.Left, Right = Keys.Right, Up = Keys.Up, Down = Keys.Down}, _Size = new Vector2(1f,1f) },
+                new Plain(texture) {_position = new Vector2(100,100), _speed = 3f, _input = new Input { Left = Keys.A, Right = Keys.D, Up = Keys.W, Down = Keys.S}, _Size = new Vector2(0.1f,0.15f), Origin = new Vector2(texture.Width/2,texture.Height/2),_rotation = 0},
+                //new Plain(texture) {_position = new Vector2(0,0), _speed = 2f, _input = new Input { Left = Keys.Left, Right = Keys.Right, Up = Keys.Up, Down = Keys.Down}, _Size = new Vector2(1f,1f) },
 
             };
 
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -78,10 +60,6 @@ namespace zZooMm001
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
--- zZooMm/zZooMm.cs
+++ zZooMm/zZooMm.cs
@@ -12,11 +12,16 @@ namespace zZooMm001
     class Plain
     {
         private Texture2D _texture;
+        public Vector2 Origin;
         public Vector2 _position;
         public Vector2 _Size;
+
         public float _speed = 2f;
         public Input _input;
 
+        public float _rotation = 3f;
+        public float _rotationSpeed = 3f;
+        public float _rotationLinear = 4f;
 
         public Plain(Texture2D texture)
         {
@@ -29,6 +34,23 @@ namespace zZooMm001
 
         public void Move()
         {
+            /////////////////////////////////////////////////////
+            //ПОВОРОТ ЗА МЫШКОЙ
+            ///////////////////
+            MouseState currentMouseState = Mouse.GetState();
+            Vector2 mousePosition = new Vector2(currentMouseState.X, currentMouseState.Y);
+
+            Vector2 direction = mousePosition - _position;
+            direction.Normalize();
+
+            _rotation =  (float)Math.Atan2((double)direction.Y, (double)direction.X) + MathHelper.ToRadians(90);// + 90 градусов из за картинки 
+
+            ///////////////////
+            //ПОВОРОТ ЗА МЫШКОЙ
+            /////////////////////////////////////////////////////
+
+            var directory = new Vector2((float)Math.Cos(MathHelper.ToRadians(90) - _rotation), -(float)Math.Sin(MathHelper.ToRadians(90) - _rotation));
+
             if (_input == null)
                 return;
             if (Keyboard.GetState().IsKeyDown(_input.Left))
@@ -41,18 +63,18 @@ namespace zZooMm001
             }
             if (Keyboard.GetState().IsKeyDown(_input.Up))
             {
-                _position.Y -= _speed;
+                _position += directory * _speed;
             }
             if (Keyboard.GetState().IsKeyDown(_input.Down))
             {
-                _position.Y += _speed;
+                _position -= directory * _speed;
             }
 
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_texture, _position, null, Color.White, 0f, new Vector2(0,0), _Size, SpriteEffects.None,0f);
+            spriteBatch.Draw(_texture, _position, null, Color.White, _rotation, Origin, _Size, SpriteEffects.None,0f);
         }
     }
 }
